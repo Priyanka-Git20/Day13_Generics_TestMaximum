f7bf5ec8aa1776fb430ba8087d4813673de9da46
priyanka <priyanka.chavan1408@gmail.com>
2022-02-25 12:04:16 +0530

    ADDED:Checking maximum float between the three.

--- test/java/com/bridgelabz/generics/MaximumTest.java
+++ test/java/com/bridgelabz/generics/MaximumTest.java
@@ -4,23 +4,22 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MaximumTest {
-
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        int finalResult = Maximum.getmaximum(9, 1, 4);
-        Assert.assertEquals(9, finalResult,0);
+    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
+        float finalResult = Maximum.getmaximum(9.6f, 1.4f, 4.4f);
+        Assert.assertEquals(9.6f, finalResult, 0.0);
     }
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        int finalResult = Maximum.getmaximum(1, 9, 4);
-        Assert.assertEquals(9, finalResult,0);
+    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
+        float finalResult = Maximum.getmaximum(1.4f, 9.6f, 4.4f);
+        Assert.assertEquals(9.6f, finalResult, 0.0);
     }
 
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        int finalResult = Maximum.getmaximum(4, 1, 9);
-        Assert.assertEquals(9, finalResult,0);
+    public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
+        float finalResult = Maximum.getmaximum(9.6f, 1.4f, 4.4f);
+        Assert.assertEquals(9.6f, finalResult, 0.0);
     }
 }
 
