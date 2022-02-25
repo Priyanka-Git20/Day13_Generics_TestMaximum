c3467abd5f805017a6d5f65462ecff7207713c68
priyanka <priyanka.chavan1408@gmail.com>
2022-02-25 12:01:30 +0530

    ADDED:Checking maximum integer between the three.

--- main/java/com/bridgelabz/generics/Maximum.java
+++ main/java/com/bridgelabz/generics/Maximum.java
@@ -1,7 +1,13 @@
 package com.bridgelabz.generics;
 
 public class Maximum {
-    public static void main(String[] args) {
-        System.out.println("welcome to the generics maximum test problem.");
+    public static Integer getmaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
+            max = b;
+        else if (c.compareTo(max) > 0)
+            max = c;
+        System.out.println(max);
+        return max;
     }
 }
---
+++ test/java/com/bridgelabz/generics/MaximumTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.generics;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MaximumTest {
+
+    @Test
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1() {
+        int finalResult = Maximum.getmaximum(9, 1, 4);
+        Assert.assertEquals(9, finalResult,0);
+    }
+
+    @Test
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue2() {
+        int finalResult = Maximum.getmaximum(1, 9, 4);
+        Assert.assertEquals(9, finalResult,0);
+    }
+
+    @Test
+    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue3() {
+        int finalResult = Maximum.getmaximum(4, 1, 9);
+        Assert.assertEquals(9, finalResult,0);
+    }
+}
+
